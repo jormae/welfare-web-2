975ebbd34e40756331d42379c0d258b1136883b4
Maheedeen Jormae <maheedeen@gmail.com>
2024-07-08 07:28:03 +0700

    update config

--- src/configs/apiConfig.js
+++ src/configs/apiConfig.js
@@ -6,7 +6,7 @@ const apiConfig = {
   // MY LOCALHOST
   // baseURL: 'http://192.168.110.166:3030'
   // LOCALHOST
-  baseURL: 'https://welfare-api.vercel.app'
+  baseURL: 'http://localhost:3030'
 
   // baseURL: process.env.REACT_APP_BASE_URI
 
